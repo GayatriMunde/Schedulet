a1ea7e9f5785f81fb9351daa0514605e9fc2f191
GayatriMunde <mundegr@gmail.com>
2021-10-03 03:04:46 +0530

    uploading stage1 done!

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import * 
+from werkzeug.debug import DebuggedApplication
+from main import app
+
+app = DebuggedApplication(app, evalex=True)
+app = Flask(__name__)
+
+@app.route('/')  
+def hello():  
+    return render_template("index.html")
+
+@app.route('/', methods=['POST'])
+def upload_file():
+    uploaded_file = request.files['file']
+    if uploaded_file.filename != '':
+        uploaded_file.save('Files/' + uploaded_file.filename)
+        return redirect('success')
+    return render_template(url_for('index'))
+
+@app.route('/success')
+def success():
+    return render_template('index.html')
+  
+if __name__ == '__main__':  
+    app.run(debug = True)  
