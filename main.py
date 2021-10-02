67835dc668d11eec7b576104de824831ed1f25aa
GayatriMunde <mundegr@gmail.com>
2021-10-03 03:06:31 +0530

    uploading  done!

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def upload_file():
 
 @app.route('/success')
 def success():
-    return render_template('index.html')
+    return render_template('success.html')
   
 if __name__ == '__main__':  
     app.run(debug = True)  
